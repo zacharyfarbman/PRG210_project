1e78570b3b830bb4d7a00cdaa5715877edbcf1f9
Zachary Farbman <zfarbman@greenkeypartners.io>
2026-07-10 12:27:44 -0400

    remove build dir

--- build/CMakeFiles/course_management_system.dir/course.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/course_management_system.dir/course.cpp.o: \
-  /Users/zacharyfarbman/Documents/Seneca/PRG210_zacharyfarbman/course_management_system/course.cpp
--- build/CMakeFiles/course_management_system.dir/interface.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/course_management_system.dir/interface.cpp.o: \
-  /Users/zacharyfarbman/Documents/Seneca/PRG210_zacharyfarbman/course_management_system/interface.cpp
