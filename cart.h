2a12209803bbb58e473d42a39b280e2214efcbd7
irbelaez <irenearbelaez@gmail.com>
2026-07-29 17:28:39 -0400

    Implement and test Cart class

--- cart.h
+++ cart.h
@@ -1,6 +1,28 @@
-class Cart {
- private:
- public:
-  Cart();
-  ~Cart();
-}
+#pragma once
+
+#include "course.h"
+#include <cstddef>
+#include <string>
+
+class Cart
+{
+private:
+    Course* courses;
+    std::string* schedules;
+    std::size_t course_count;
+    double total_cost;
+
+    void calculateTotalCost();
+
+public:
+    Cart();
+    ~Cart();
+
+    void addCourse(const Course& course);
+    void displayCourses() const;
+    void clearCart();
+
+    bool isEmpty() const;
+    std::size_t getCourseCount() const;
+    double getTotalCost() const;
+};
