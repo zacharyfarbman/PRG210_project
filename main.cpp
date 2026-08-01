c831f6bb4e7542a096f6723d5b9d119f363bf49d
irbelaez <irenearbelaez@gmail.com>
2026-08-01 18:30:15 -0400

    cleanup

--- main.cpp
+++ main.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 
-#include "cart.h"
 #include "course.h"
 #include "department.h"
-#include "admin_interface.h"
 #include "student_interface.h"
 #include "CSVUtils.h"
 
@@ -14,33 +12,7 @@ const char* csvFile = "courses.csv";
 
 int main() {
 
-  Cart cart;
-
-  const Course course1("PRG210", "Programming 2", "M/W", 500.00);
-
-  const Course course2("NTF201", "Networking Fundamentals", "T/R", 450.00);
-
-  std::cout << "Course Management System\n";
-
-  cart.addCourse(course1);
-  cart.addCourse(course2);
-
-  cart.displayCourses();
-
-  std::cout << "Course count: " << cart.getCourseCount() << '\n';
-
-  std::cout << std::boolalpha << "Cart is empty: " << cart.isEmpty() << '\n';
-
-  cart.clearCart();
-
-  std::cout << "Course count after clearing: " << cart.getCourseCount() << '\n';
-
-  std::cout << std::boolalpha
-            << "Cart is empty after clearing: " << cart.isEmpty() << '\n';
   loadFromCSV(csvFile);
-
-  //AdminInterface admin;
-  //admin.run();
 
   StudentInterface student;
   student.run();
